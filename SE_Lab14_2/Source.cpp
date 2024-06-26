641ae7d4d61415c5bc6e306d5e9b422ed73fa21e
Thying <152802890+Thying@users.noreply.github.com>
2024-06-26 08:35:32 +0300

    тест In::getin работает

--- SE_Lab14_2/Source.cpp
+++ SE_Lab14_2/Source.cpp
@@ -8,17 +8,17 @@ int main(int argc, char* argv[]) {
 
     std::cout << "- - - тест In::getin" << std::endl << std::endl;
 
-try
-{
-    char a[] = "C:/Games/in.txt";
-    In::IN in = In::getin(a);
-    std::cout << in.text << std::endl;
-    std::cout << "Всего символов: " << in.size << std::endl;
-    std::cout << "Всего строк: " << in.lines << std::endl;
-    std::cout << "Пропущено: " << in.ignor << std::endl;
-}
-catch (Error::ERROR e)
-{
-    std::cout << "Ошибка" << e.id << ": " << e.message << std::endl << std::endl;
-}
+    try
+    {
+        Parm::PARM parm = Parm::getparm(argc, argv);
+        In::IN in = In::getin(parm.in);
+        std::cout << in.text << std::endl;
+        std::cout << "Всего символов: " << in.size << std::endl;
+        std::cout << "Всего строк: " << in.lines << std::endl;
+        std::cout << "Пропущено: " << in.ignor << std::endl;
+    }
+    catch (Error::ERROR e)
+    {
+        std::cout << "Ошибка" << e.id << ": " << e.message << std::endl << std::endl;
+    }
 }
